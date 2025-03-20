345e1799d484410f9572366d98913dc2a0865a8b
Eyüp Mustafa YILDIRIM <eyupyldrm07@gmail.com>
2025-03-20 23:30:53 +0300

    Add project files.

---
+++ ApiProjeKampi.WepApi/Entities/Chef.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi.WepApi.Entities
+{
+    public class Chef
+    {
+        public int ChegId { get; set; }
+        public string  NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ ApiProjeKampi.WepApi/Entities/Rezarvation.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiProjeKampi.WepApi.Entities
+{
+    public class Rezarvation
+    {
+        public int ReservationId { get; set; }
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime RezarvationDate { get; set; }
+        public string RezarvationTime { get; set; }
+        public int CountOfPeople { get; set; }
+        public string Message { get; set; }
+        public string RezarvationStatus { get; set; }
+    }
+}
